5b29a1f49aa247385a14c9d1a0b3e52a4ac164c2
John Cochran <mr.johnmcochran@gmail.com>
2024-03-22 19:31:57 -0400

    added routing for getting and creating users

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const db = require('./queries')
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
 app.use(express.static('dist')); // Assuming your Vue.js app is in a 'dist' folder
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ 
+  extended: true, 
+  })
+)
+
+
+app.get('/', (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API"})
+});
+
+app.get('/users/:username', db.getUserByUsername)
+app.post('/users', db.createUser)
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
