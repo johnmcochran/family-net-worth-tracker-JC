8542ebf5820706f56a43895b837cfb6426cbe9d6
John Cochran <mr.johnmcochran@gmail.com>
2024-07-29 00:20:40 -0400

    imported PrimeVue for components

--- family_net_worth_tracker_app/src/main.js
+++ family_net_worth_tracker_app/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).use(router).mount('#app')
+createApp(App).use(router).use(router).mount('#app').use(PrimeVue,
+    {
+        theme: {
+            preset: Aura
+        }
+    }
+);
